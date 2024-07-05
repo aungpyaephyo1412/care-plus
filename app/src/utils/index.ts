26001f8bf3a7a46b2628e7f996d836c43f0c6ffb
Aung Pyae Phyo <115131869+aungpyaephyo1412@users.noreply.github.com>
2024-07-05 07:21:35 +0630

    init: project setup with nestjs and react native

---
+++ app/src/utils/index.ts
@@ -0,0 +1,9 @@
+import clsx, {ClassValue} from "clsx";
+import {twMerge} from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+    return twMerge(clsx(inputs));
+}
+
+export const blurhash =
+    '|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[';
